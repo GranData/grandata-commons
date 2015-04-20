70ffb4fd3f134626e1fafe8f769179f36b4b7704
Marcos Ariel <marcos@grandata.com>
2015-04-20 10:46:03 -0300

    Update package.scala

--- src/main/scala/com/grandata/commons/strings/package.scala
+++ src/main/scala/com/grandata/commons/strings/package.scala
@@ -7,5 +7,13 @@ package object strings {
   implicit class RichSplitableString[T](s: String) {
     def splitFields(char: Char) = s.split(s"\\$char", -1)
   }
+  
+  def zerosWhenEmpty(columns: List[String])={
+    columns.map(zeroIfEmpty(_))
+  }
+
+  def zeroIfEmpty(value:String)={
+    if (!value.trim.isEmpty) value else 0.toString
+  }
 
 }
