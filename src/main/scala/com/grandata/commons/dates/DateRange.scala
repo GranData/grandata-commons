fa47a2267a79d8d0921db917c2f1f35f4e1a714b
Gustavo Ajzenman <gustavoajz@gmail.com>
2015-04-20 12:18:50 -0300

    Plus Months

--- src/main/scala/com/grandata/commons/dates/DateRange.scala
+++ src/main/scala/com/grandata/commons/dates/DateRange.scala
@@ -1,10 +1,15 @@
 package com.grandata.commons.dates
 
-import org.joda.time.LocalDate
+import org.joda.time.{DateTime, LocalDate}
 
 /**
  * Created by gustavo on 16/04/15.
  */
+
+object DateRange {
+  def plusMonths(start: LocalDate, months: Int) = new DateRange(start, start.plusMonths(months))
+}
+
 class DateRange(val start: LocalDate, val end: LocalDate) {
   def dayIterator = DateUtils.dayIterator(start, end)
   def monthIterator = DateUtils.monthIterator(start, end)
