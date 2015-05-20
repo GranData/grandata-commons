c5e92925bab34979c7f8f24b20903f3eca523f12
Esteban Donato <esteban.donato@gmail.com>
2015-05-20 20:07:08 -0300

    FileUtils: renaming parameter names, removing unused functions and adding more test cases

--- src/main/scala/com/grandata/commons/files/FileUtils.scala
+++ src/main/scala/com/grandata/commons/files/FileUtils.scala
@@ -5,6 +5,9 @@ import java.io._
 import java.util.zip.{GZIPOutputStream, GZIPInputStream}
 import org.apache.commons.io.{ FileUtils => ApacheFileUtils }
 import scala.io.Codec
+import java.nio.file.Files
+import scala.util.Try
+import scala.collection.JavaConversions._
 
 /** Utility functions for managing files and resources in different formats i.e.: text, binary, gzip, etc.
  *  All the I/O operations are encoded in UTF-8
@@ -18,84 +21,58 @@ object FileUtils {
    */
   implicit val codec: Codec = Codec.UTF8
 
-  trait StreamConverter {
-    def buildStream(f: File): OutputStream
-  }
-
-
-  object IdentityStreamConverter extends StreamConverter {
-    def buildStream(f: File): OutputStream = ApacheFileUtils.openOutputStream(f)
-  }
-
-  object GzStreamConverter extends StreamConverter {
-    def buildStream(f: File): OutputStream = new GZIPOutputStream(ApacheFileUtils.openOutputStream(f))
-  }
-
-  def write(filePath: String, content: String): Unit = printToFile(new File(filePath), content)
-  def printToFile(f: String, c: String): Unit = printToFile(new File(f), c)
+  def printToFile(file: String, content: String): Unit = printToFile(new File(file), content)
   
   /** saves content to a text file
-    * @param f: file to save the content
-    * @param c: content to be saved
+    * @param file: file to save the content
+    * @param content: content to be saved
     */
-  def printToFile(f: File, c: String): Unit = getFiltePrinter(f) { p=> p.println(c) }
-  def printToFile(f: String, c: Iterable[String]): Unit = printToFile(new File(f), c)
+  def printToFile(file: File, content: String): Unit = printToFile(file, List(content))
+  def printToFile(file: String, content: Iterable[String]): Unit = printToFile(new File(file), content)
   
   /** saves content to a text file
-    * @param f: file to save the content
-    * @param c: content to be saved. Each element of the Iterable represents a new line in the file
+    * @param file: file to save the content
+    * @param content: content to be saved. Each element of the Iterable represents a new line in the file
     */
-  def printToFile(f: File, c: Iterable[String]): Unit = getFiltePrinter(f) { p=> c.foreach(p.println(_)) }
-
-  def getFiltePrinter(f: String)(op: java.io.PrintWriter => Unit): Unit =
-    getFiltePrinter(new File(f)) _
-
-
-  def getFiltePrinter(f: File)(op: java.io.PrintWriter => Unit)(implicit converter: StreamConverter = IdentityStreamConverter): Unit = {
-    val p = new java.io.PrintWriter(converter.buildStream(f))
-    try {
-      op(p)
-    } finally {
-      p.close()
-    }
-  }
+  def printToFile(file: File, content: Iterable[String]): Unit = 
+    ApacheFileUtils.writeLines(file, codec.name, content)
   
   /**
     * reads content from a text file
-    * @param f file path
+    * @param file file path
     * @return file content as String 
     */
-  def fileContent(f: String) = fromFile(f).mkString
+  def fileContent(file: String) = Try(fromFile(file).mkString).getOrElse("").trim
   
   /**
     * reads content from a text file
-    * @param f file path
+    * @param file file path
     * @return file content as Iterator[String]. Each element of the iterator represents one line in the file
     */
-  def fileLines(f: String) = fromFile(f).getLines()
+  def fileLines(file: String) = Try(fromFile(file).getLines()).getOrElse(Iterator.empty)
   
   /**
     * reads content from a gzipped file
-    * @param f file path
+    * @param file file path
     * @return file content as String 
     */
-  def gzippedFileContent(f: String) =
-    fromInputStream(new GZIPInputStream(new BufferedInputStream(new FileInputStream(f)))).mkString
+  def gzippedFileContent(file: String) =
+    Try(fromInputStream(new GZIPInputStream(new BufferedInputStream(new FileInputStream(file)))).mkString).getOrElse("").trim
   
   /**
     * read content from a gzipped file
-    * @param f file path
+    * @param file file path
     * @return file content as Iterator[String]. Each element of the iterator represents one line in the file
     */
-  def gzippedFileLines(f: String) =
-    fromInputStream(new GZIPInputStream(new BufferedInputStream(new FileInputStream(f)))).getLines()
+  def gzippedFileLines(file: String) =
+    Try(fromInputStream(new GZIPInputStream(new BufferedInputStream(new FileInputStream(file)))).getLines()).getOrElse(Iterator.empty)
   
   /** reads content from a resource
-    * @param f resource path
+    * @param resource resource path
     * @return resource content as String 
     */
-  def resourceContent(r: String) = {
-   val url = getClass.getResource(r)
-   if(url != null) fromURL(url).mkString else "" 
+  def resourceContent(resource: String) = {
+   val url = getClass.getResource(resource)
+   if(url != null) fromURL(url).mkString else ""
   }
 }
--- src/test/scala/com/grandata/commons/files/FileUtilsSpec.scala
+++ src/test/scala/com/grandata/commons/files/FileUtilsSpec.scala
@@ -1,15 +1,119 @@
 package com.grandata.commons.files
 
 import org.specs2.mutable.Specification
+import java.nio.file.Paths
+import java.nio.file.Files
+import org.apache.commons.io.{ FileUtils => ApacheFileUtils }
+import java.util.zip.GZIPOutputStream
+import java.nio.file.Path
 
 class FileUtilsSpec extends Specification {
+  
+  val tmpDir = sys.props("java.io.tmpdir")
   
   "FileUtils" should {
     "return content from a resource file" in {
       FileUtils.resourceContent("/fileUtils.resource") === "resource content"
     }
+    
     "return empty from an inexistent resource file" in {
       FileUtils.resourceContent("/notExist.resource") isEmpty
     }
+    
+    "print content to a text file" in {
+      val path = Paths.get(tmpDir, "FileUtilsSpec")
+      try {
+        FileUtils.printToFile(path.toString, "test content")
+        FileUtils.fileContent(path.toString) === "test content"  
+      } finally {
+        Files.deleteIfExists(path)  
+      }
+    }
+    
+    "print list of lines to a text file" in {
+      val path = Paths.get(tmpDir, "FileUtilsSpec1")
+      try {
+        FileUtils.printToFile(path.toString, List("line1", "line2"))
+        FileUtils.fileContent(path.toString) === "line1\nline2"  
+      } finally {
+        Files.deleteIfExists(path)  
+      }
+    }
+    
+    "print content to a text file in an inexistent sub-directory" in {
+      val path = Paths.get(tmpDir,"newdir", "FileUtilsSpec2")
+      try {
+        FileUtils.printToFile(path.toString, "new content")
+        FileUtils.fileContent(path.toString) === "new content"  
+      } finally {
+        ApacheFileUtils.deleteDirectory(Paths.get(tmpDir,"newdir").toFile)  
+      }
+    }
+    
+    "return content from a text file as list of lines" in {
+      val path = Paths.get(tmpDir, "FileUtilsSpec3")
+      try {
+        FileUtils.printToFile(path.toString, List("line1", "line2"))
+        FileUtils.fileLines(path.toString) must contain(exactly("line1", "line2")).inOrder
+      } finally {
+        Files.deleteIfExists(path)
+      }
+    }
+    
+    "return empty from an inexistent file" in {
+      FileUtils.fileContent("/notExist.txt") isEmpty
+    }
+    
+     "return empty list from an inexistent file" in {
+      FileUtils.fileLines("/notExist.txt") isEmpty
+    }
+     
+    "return content from a gzipped text file" in {
+      val path = Paths.get(tmpDir, "FileUtilsSpec4")
+      generateZipFile(path, "zipped content")
+      try {
+        FileUtils.gzippedFileContent(path.toString) === "zipped content"
+      } finally {
+        Files.deleteIfExists(path)
+      }
+    }
+    
+    "return content from a gzipped text file as list of lines" in {
+      val path = Paths.get(tmpDir, "FileUtilsSpec5")
+      generateZipFile(path, "zipped line1\nzipped line2")
+      try {
+        FileUtils.gzippedFileLines(path.toString) must contain(exactly("zipped line1", "zipped line2")).inOrder
+      } finally {
+        Files.deleteIfExists(path)
+      }
+    }
+    
+    "return empty from an invalid gzipped file" in {
+      val path = Paths.get(tmpDir, "FileUtilsSpec6")
+      FileUtils.printToFile(path.toString, List("line1", "line2"))
+      try {
+        FileUtils.gzippedFileContent(path.toString) isEmpty
+      } finally {
+        Files.deleteIfExists(path)
+      }
+    }
+    
+    "return empty list from an invalid gzipped file" in {
+      val path = Paths.get(tmpDir, "FileUtilsSpec7")
+      FileUtils.printToFile(path.toString, List("line1", "line2"))
+      try {
+        FileUtils.gzippedFileLines(path.toString) isEmpty
+      } finally {
+        Files.deleteIfExists(path)
+      }
+    }
   }
+  
+  def generateZipFile(p: Path, c: String) {
+    val gzip = new GZIPOutputStream(Files.newOutputStream(p))
+    gzip.write(c.getBytes)
+    gzip.finish()
+    gzip.close()
+  }
+  
 }
