3a6a1cb569c170999b0149ec666396fb0cc76cf1
mfoglino <mfoglino@gmail.com>
2015-06-03 20:20:48 -0300

    better inheritance for Config

--- src/main/scala/com/grandata/commons/config/Config.scala
+++ src/main/scala/com/grandata/commons/config/Config.scala
@@ -1,11 +1,16 @@
 package com.grandata.commons.config
 
+import java.util.Date
+
 import com.typesafe.config.ConfigFactory
 import java.io.File
 
 
 trait Config {
   val applicationName: String
+
+  def customer: String
+
 
   private val homeDir = System.getProperty("user.home")
 
@@ -15,4 +20,13 @@ trait Config {
 
   lazy val userApp = ConfigFactory.parseFile(new File(homeDir + "/" + appConfigFile))
   lazy val application = userApp.withFallback(devApplicationConf)
+
+
+  def buildInputPath(date: Date, `type`: String, pattern: String = "*"): String
+
+  def buildInputPath(`type`: String, pattern: String): String
+
+  def buildOutputPath(date: Date, `type`: String, dir: String = ""): String
+
+  def buildSingleOutputPath(date: Date, `type`: String, dir: String = ""): String
 }
