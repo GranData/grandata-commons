8fc2321f61350b551dbad6371051c9ab5359d10a
mfoglino <mfoglino@gmail.com>
2015-06-06 11:58:40 -0300

    refactor config aganin

--- src/main/scala/com/grandata/commons/config/Config.scala
+++ src/main/scala/com/grandata/commons/config/Config.scala
@@ -1,16 +1,11 @@
 package com.grandata.commons.config
 
-import java.util.Date
-
-import com.typesafe.config.ConfigFactory
 import java.io.File
 
+import com.typesafe.config.ConfigFactory
 
 trait Config {
   val applicationName: String
-
-  def customer: String
-
 
   private val homeDir = System.getProperty("user.home")
 
@@ -20,13 +15,4 @@ trait Config {
 
   lazy val userApp = ConfigFactory.parseFile(new File(homeDir + "/" + appConfigFile))
   lazy val application = userApp.withFallback(devApplicationConf)
-
-
-  def buildInputPath(date: Date, `type`: String, pattern: String = "*"): String
-
-  def buildInputPath(`type`: String, pattern: String): String
-
-  def buildOutputPath(date: Date, `type`: String, dir: String = ""): String
-
-  def buildSingleOutputPath(date: Date, `type`: String, dir: String = ""): String
 }
