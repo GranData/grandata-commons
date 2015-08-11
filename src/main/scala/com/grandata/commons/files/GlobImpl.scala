a70aa365f0e39cbe8eb2a7b607f7bf0f27d2849a
Gustavo Ajzenman <gustavoajz@gmail.com>
2015-08-11 18:01:03 -0300

    Glob add missing case: */*/concrete_file

--- src/main/scala/com/grandata/commons/files/GlobImpl.scala
+++ src/main/scala/com/grandata/commons/files/GlobImpl.scala
@@ -6,6 +6,7 @@ import java.nio.file.FileSystem
 
 import scala.util.Try
 
+import java.nio.file.Path
 /**
  * Created by gustavo on 26/03/15.
  */
@@ -22,6 +23,15 @@ trait GlobImpl {
     private lazy val magicRegex = """[*?\[\{]""".r
     private def withoutMagic(pattern: String): Boolean = magicRegex.findFirstIn(pattern).isEmpty
 
+    private def processPath(magic: Array[String], foundPath: Path): Iterator[String] = {
+      if (magic.size <= 1)
+        Iterator(foundPath.toString)
+      else if (Files.isDirectory(foundPath))
+        iglob(foundPath.toString, magic.tail)
+      else
+        Iterator.empty
+
+    }
     private def iglob(basedir: String, patterns: Array[String]): Iterator[String] = {
 
       if (patterns.isEmpty) Iterator.empty
@@ -29,28 +39,26 @@ trait GlobImpl {
         val (noMagic, magic) = patterns.span(withoutMagic)
         val newBase = fileSystem.getPath(basedir, noMagic.mkString("/"))
 
-//        println(s"basedir: $basedir, newBase: $newBase, magic: ${magic.mkString(" / ")}")
+        magic.headOption match {
+          case Some(magicHead) =>
+            Try(Files.newDirectoryStream(newBase, magicHead))
+              .map(stream =>
+                try {
+                  stream.map { foundPath =>
+                    processPath(magic, foundPath)
+                  }.fold(Iterator.empty)(_ ++ _)
+                } finally {
+                  stream.close()
+                }
+              ).getOrElse(Iterator.empty)
+          case None =>
+            Iterator(newBase.toString)
+        }
 
-        Try(Files.newDirectoryStream(newBase, magic.head))
-          .map(stream =>
-          try {
-            stream.map { foundPath =>
-              if (magic.tail.isEmpty)
-                Iterator(foundPath.toString)
-              else if (Files.isDirectory(foundPath))
-                iglob(foundPath.toString, magic.tail)
-              else
-                Iterator.empty
-            }.fold(Iterator.empty)(_ ++ _)
-          } finally {
-            stream.close()
-          }
-          ).getOrElse(Iterator.empty)
       }
     }
 
     def glob(pattern: String): Iterator[String] = {
-      //    println(s"Exists: ${Files.exists(FileSystems.getDefault.getPath(pattern))} // ${withoutMagic(pattern)}")
       if (withoutMagic(pattern)) {
         if (Files.exists(fileSystem.getPath(pattern)))
           Iterator(pattern)
