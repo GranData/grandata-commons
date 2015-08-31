083351d55c3dd3d3c68539ee22af54b927223667
Esteban Donato <esteban.donato@gmail.com>
2015-08-31 02:09:54 -0300

    changing behavior of Config trait. It loads config from the default behavior and use the resource as a fallback

--- src/main/scala/com/grandata/commons/config/Config.scala
+++ src/main/scala/com/grandata/commons/config/Config.scala
@@ -6,13 +6,6 @@ import com.typesafe.config.ConfigFactory
 
 trait Config {
   val applicationName: String
-
-  private val homeDir = System.getProperty("user.home")
-
-  private lazy val devApplicationConf = ConfigFactory.load("application")
-  private lazy val appConfigFile =
-    devApplicationConf.getString(s"${applicationName}.application.app-config-path")
-
-  lazy val userApp = ConfigFactory.parseFile(new File(homeDir + "/" + appConfigFile))
-  lazy val application = userApp.withFallback(devApplicationConf)
+  
+  lazy val applicationConf = ConfigFactory.load.withFallback(ConfigFactory.load("application"))
 }
